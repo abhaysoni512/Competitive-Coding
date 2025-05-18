9c9755b05e6aced964ec25d889f90755156f258e
Abhay Soni <abhaysoni512@gmail.com>
2025-05-18 10:41:33 +0530

    Added: 1_basic.cpp

--- 1_Recursion/1_basic.cpp
+++ 1_Recursion/1_basic.cpp
@@ -1,7 +1,32 @@
+/*
+  Recursion :
+    - A function that calls itself is called a recursive function.
+    - A recursive function must have a base case to stop the recursion.
+
+  Terminlogy :
+    - Base case : The condition under which the recursion stops.
+    - Recursive case : The condition under which the recursion continues.
+    - Stack Space : The amount of memory used by the function calls.
+    - Stack Overflow : When the stack space is exceeded, it results in a stack overflow error.
+    - Tail Recursion : A recursive function where the recursive call is the last statement in the function.
+    - Recurision Tree : A tree that represents the recursive calls made by a function.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+auto printN(string_view s, int & n){
+    if(n == 0) return "";
+    n--;
+    return printN(s, n);
+    
+}
 
 int main(){
-    
+    static int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string result = printN(s, n);
+    cout << result << endl;
     return 0;
-}   
+}
